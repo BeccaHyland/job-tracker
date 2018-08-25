abab2d95afe7e4328f66c45346dc7acad92068ed
Tim Fielder <fiel1139@gmail.com>
2018-08-25 14:52:01 -0600

    added pry to gems.  Added two tests for user clicks on link to see job show page.

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -11,4 +11,22 @@ describe "User sees a specific job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
   end
+  scenario "a user sees a link for a job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+    visit company_job_path(company, job)
+    
+    expect(page).to have_link(job.title)
+  end
+  scenario "a user clicks on a link for a job to see the job show page" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+    visit company_job_path(company, job)
+
+    #click on the link and
+
+    #expect page path to be job id show page
+  end
 end
