f095830ad602b948dc65d642b1e76e2be3d6577e
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-25 16:12:09 -0600

    WIP create route action, error undefined variable category_params

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,18 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "#{@category.title} added!"
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
 end
