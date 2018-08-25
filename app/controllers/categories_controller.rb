062309281e666f12a54f3ba372161532eb62b796
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-25 17:03:54 -0600

    remember to finish the create alternate flash message

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,14 +13,16 @@ class CategoriesController < ApplicationController
       flash[:success] = "#{@category.title} added!"
       redirect_to category_path(@category)
     else
+      # flash[] - figure out what else to put here for the sorry message - see saved bookmark
       render :new
     end
   end
 
   def show
     category = Category.find(params[:id])
+    redirect_to categories_path(category)
   end
-  
+
   private
 
   def category_params
