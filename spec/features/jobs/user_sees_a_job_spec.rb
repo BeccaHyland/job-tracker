78b6d22246ee484d7af4c5aee99f00617d16ca0a
Tim Fielder <fiel1139@gmail.com>
2018-08-25 15:57:34 -0600

    moves tests from job to companies because it is from the company page to access an individual job.

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -11,22 +11,4 @@ describe "User sees a specific job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
   end
-  scenario "a user sees a link for a job" do
-    company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-
-    visit company_job_path(company, job)
-
-    expect(page).to have_link(job.title)
-  end
-  scenario "a user clicks on a link for a job to see the job show page" do
-    company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-
-    visit company_job_path(company, job)
-
-    click_link("#{job.title}")
-
-    expect(page).to have_content("#{job.title}")
-  end
 end
