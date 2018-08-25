a86a0dacd370bf9202955a7635a5c461e81d3af4
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-25 15:32:31 -0600

    adds spec file and draft create category test

---
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'user creates a new category' do
+  describe 'they link from the categories index' do
+    describe 'they fill in a title' do
+      it 'creates a new category' do
+        visit categories_path
+        click_link "Create New Category"
+
+        expect(current_path).to eq(new_category_path)
+
+        fill_in "category[title]", with: "New Test Category"
+        click_on "Create Category"
+
+        #expect path
+        expect(page).to have_content("New Test Category")
+      end
+    end
+  end
+end
