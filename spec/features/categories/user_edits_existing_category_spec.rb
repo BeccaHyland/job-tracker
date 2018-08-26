4c4a954dfb713879e3de8a5bb23770e0dd9166fd
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-26 11:27:46 -0600

    adds spec for edit category

---
+++ spec/features/categories/user_edits_existing_category_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "User edits an existing category" do
+  scenario "a user can edit a category" do
+    category = Category.create!(title: "Financial")
+    visit edit_category_path(category)
+
+    fill_in "category[title]", with: "Fin. Private Sector"
+    click_button "Update"
+
+    expect(current_path).to eq("/categories/#{Category.last.id}")
+    expect(page).to have_content("Fin. Private Sector")
+    expect(page).to_not have_content("Financial")
+  end
+end
