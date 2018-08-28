4772bfcaac89f9b1b9b430624346944c1d9f9c4a
Tim Fielder <fiel1139@gmail.com>
2018-08-28 11:27:32 -0600

    adds test for create form for job comment.

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -15,8 +15,9 @@ describe "User sees a specific job" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
 
-    visit company_job_path(company, job)
+    visit job_path(job)
 
-    
+    expect(page).to have_content("Notes")
+    expect(page).to have_content("Save")
   end
 end
