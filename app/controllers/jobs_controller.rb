d523130e45d2116502a6a1ae34ff8049519d43de
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-27 19:00:45 -0600

    ATP for jobs update functionality

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,11 +25,20 @@ class JobsController < ApplicationController
   end
 
   def edit
-    # implement on your own!
+    @company = Company.find(params[:company_id])
+    @job = Job.find(params[:id])
   end
 
   def update
-    # implement on your own!
+    @company = Company.find(params[:company_id])
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    if @job.save
+      flash[:success] = "#{@job.title} updated!"
+      redirect_to company_job_path(@company, @job)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -39,6 +48,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :level_of_interest, :city)
+    params.require(:job).permit(:title, :description, :level_of_interest, :city, :company_id)
   end
 end
