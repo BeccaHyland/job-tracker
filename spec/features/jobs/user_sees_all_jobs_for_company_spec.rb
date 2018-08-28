dbb995d489d3f919ad84258a7cc00a2bc7e31ba2
Tim Fielder <fiel1139@gmail.com>
2018-08-27 19:50:03 -0600

    finishes up company job page.

--- spec/features/jobs/user_sees_all_jobs_for_company_spec.rb
+++ spec/features/jobs/user_sees_all_jobs_for_company_spec.rb
@@ -18,17 +18,17 @@ describe "On company job page" do
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
 
     visit company_jobs_path(company)
-    
+
     expect(page).to have_link(job.title)
   end
-  # scenario "a user clicks on a link for a job to see the job show page" do
-  #   company = Company.create!(name: "ESPN")
-  #   job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-  #
-  #   visit company_path(company)
-  #
-  #   click_link(job.title)
-  #
-  #   expect(page).to have_content("#{job.title}")
-  # end
+  scenario "a user clicks on a link for a job to see the job edit page" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+    visit company_jobs_path(company)
+
+    click_link(job.title)
+
+    expect(current_path).to eq(edit_company_job_path(company, job))
+  end
 end
