3fee088f59e06cd857604c596c2cdf039831b137
Tim Fielder <fiel1139@gmail.com>
2018-08-29 09:01:45 -0600

    adds the comment for jobs functionality.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,29 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    @comment = Comment.find(params[:id])
+    @job = @comment.job
+  end
 
+  def destroy
+    job = Job.find(params[:id])
+    comment = Comment.find(params[:id])
+    comment.destroy
+
+    flash[:success] = "#{contact.name} was successfully deleted."
+    redirect_to job_path(job)
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    job = @comment.job
+    @comment.update(comment_params)
+    if @comment.save
+      flash[:success] = "#{@comment.name} updated!"
+      redirect_to job_path(job)
+    else
+      render :edit
+    end
   end
 
   private
