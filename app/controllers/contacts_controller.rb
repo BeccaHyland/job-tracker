83ffb0e1a356a8f0a1631fbd51eddd3ccf1b50e9
Tim Fielder <fiel1139@gmail.com>
2018-08-28 19:31:10 -0600

    stopping point.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,16 +8,16 @@ class ContactsController < ApplicationController
 
   def edit
     @contact = Contact.find(params[:id])
-    # redirect_to company_path(@contact.company)
+    redirect_to edit_contact_path(@contact.company)
   end
 
   def destroy
     company = Company.find(params[:id])
     contact = Contact.find(params[:id])
     contact.destroy
-
+    
     flash[:success] = "#{contact.name} was successfully deleted."
-    company_path(company)
+    redirect_to company_path(company)
   end
 
   private
