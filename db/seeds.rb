9c9e3a11dbc38682acc37c8e9ecd7b6e2c6bff10
Tim Fielder <fiel1139@gmail.com>
2018-08-28 23:24:33 -0600

    revised seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,12 @@ Job.destroy_all
 COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
 JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
-CATEGORIES = ["Fun", "Delightful", "Soothing", "Peaceful"]
+CATEGORIES = ["Fun", "Delightful", "Soothing", "Peaceful", "Three", "You", "Me", "SomeofUs", "NIne", "Ten"]
 
 
 COMPANIES.each do |name|
   company = Company.create!(name: name)
-  category = Category.create!(title: CATEGORIES.sample)
+  category = Category.create!(title: CATEGORIES[company.id - 1])
   puts "Created #{company.name}"
   10.times do |num|
     company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category: category)
