0a39307a9fa7d14b3a78c0542a1cc59ffbdb6085
Tim Fielder <fiel1139@gmail.com>
2018-08-28 23:15:04 -0600

    updated seeds and removed uniqueness requirement from category.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,10 @@ CATEGORIES = ["Fun", "Delightful", "Soothing", "Peaceful"]
 
 COMPANIES.each do |name|
   company = Company.create!(name: name)
+  category = Category.create!(title: CATEGORIES.sample)
   puts "Created #{company.name}"
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category: CATEGORIES.sample)
+    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category: category)
     puts "  Created #{company.jobs[num].title}"
   end
 end
