208a9498a07aa59d27963913fa6bf7f46d2e9d8c
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-28 20:38:18 -0600

    populates category jobs on category show, one convention errors in view awaiting coaching

--- spec/features/categories/user_sees_one_category_spec.rb
+++ spec/features/categories/user_sees_one_category_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 describe "User sees one category" do
-  scenario "a user sees a category" do
-    category = Category.create!(title: "Energy")
-    #category.jobs.create!(title: "Developer", level_of_interest: 100, city: "Boulder")
+  it "shows a category" do
+    category = Category.create!(title: "Entertainment")
 
     visit category_path(category)
 
     expect(current_path).to eq(category_path(category))
-    expect(page).to have_content("Energy")
+    expect(page).to have_content("Entertainment")
+  end
+
+  it "shows all the jobs in one category" do
+    category = Category.create!(title: "Entertainment")
+    company = Company.create(name: "Denver Aquarium")
+    job1 = company.jobs.create(title: "Jr. Developer", level_of_interest: 70, city: "Denver", category: category)
+    job2 = company.jobs.create(title: "DBA", level_of_interest: 20, city: "Denver", category: category)
+
+    visit category_path(category)
+
+    expect(current_path).to eq(category_path(category))
+    expect(page).to have_link("Jr. Developer")
+    expect(page).to have_link("DBA")
   end
 end
